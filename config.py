d89e2da7e0bceddebdd7819a6e5a9ccef1d90e14
lexer697 <music1ygffygfgdf@gmail.com>
2025-10-10 20:48:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/lexer697/Aura",
+    "https://github.com/lexer697/Badwa",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
